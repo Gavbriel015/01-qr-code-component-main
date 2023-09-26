af7328b577e270fb381b5236e330d6077df60261
Gavbriel <av.gabriel015@gmail.com>
2023-09-26 18:39:14 -0400

    Primer Proyecto de FrontEndMentor

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        background: '#d6e2f0',
+      }
+    },
+  },
+  plugins: [],
+}
+
